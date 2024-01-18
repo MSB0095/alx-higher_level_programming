2ee2910f132fa156ae5302d503778de2cb1550cf
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-01-18 21:09:24 +0100

    best i could

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,18 +5,18 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
     a = int(sys.argv[1])
-    b = sys.argv[2]
-    c = int(sys.argv[1])
-    if b == "+":
-        print("{} + {} = {}".format(a, c, add(a, c)))
-    elif b == "-":
-        print("{} - {} = {}".format(a, c, sub(a, c)))
-    elif b == "*":
-        print("{} * {} = {}".format(a, c, mul(a, c)))
-    elif b == "/":
-        print("{} / {} = {}".format(a, c, div(a, c)))
+    o = sys.argv[2]
+    b = int(sys.argv[3])
+    if o == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif o == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif o == "\*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif o == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
