36477941413c810b6439250b5f8622206ac1ad6d
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-01-18 21:12:35 +0100

    fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif o == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif o == "\*":
+    elif o == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
     elif o == '/':
         print("{} / {} = {}".format(a, b, div(a, b)))
