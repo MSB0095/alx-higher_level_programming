42cb2e02176180e5e3c5b77f7dff12e64aff1eac
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-01-22 19:30:46 +0100

    Refactor new_in_list function to improve readability and remove unnecessary condition checks

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if my_list and idx and element is not None:
-        if idx < 0 or idx > (len(my_list) - 1):
-            new_list = my_list.copy()
-            return new_list
+    if idx < 0 or idx > (len(my_list) - 1):
         new_list = my_list.copy()
-        new_list[idx] = element
         return new_list
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
