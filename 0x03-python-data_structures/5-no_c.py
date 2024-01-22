64c2d09d6c525bdaf6b11c99f7ff5bc9d5e5e35b
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-01-23 00:27:31 +0100

    Add solution for removing 'c' from a string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def no_c(my_string):
+    if my_string:
+        new_string = []
+        for i in range(len(my_string)):
+            if (my_string[i] == 'c') or (my_string[i] == 'C'):
+                continue
+            new_string.append(my_string[i])
+        new_string = ''.join(new_string)
+        return new_string
