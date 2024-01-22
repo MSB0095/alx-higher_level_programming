15aad20f4f672969fc956c4098894e38e20858ee
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-01-22 19:23:23 +0100

    Fix index condition in new_in_list function

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx < (len(my_list) - 1):
+    if idx < 0 or idx > (len(my_list) - 1):
         new_list = my_list
         return new_list
     new_list = my_list
