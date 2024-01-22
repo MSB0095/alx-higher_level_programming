9e8e4466a5b36d3aa91f03bbec2d4bc7a13edeb0
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-01-22 19:27:33 +0100

    Fix index out of range issue in new_in_list function

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(my_list) - 1):
-        new_list = my_list
+    if my_list and idx and element:
+        if idx < 0 or idx > (len(my_list) - 1):
+            new_list = my_list.copy()
+            return new_list
+        new_list = my_list.copy()
+        new_list[idx] = element
         return new_list
-    new_list = my_list
-    new_list[idx] = element
-    return new_list
