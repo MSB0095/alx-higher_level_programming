6824066dbc0c1b4984ae60bcfd8bf81e660085cd
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-01-23 00:35:45 +0100

    Refactor no_c function to use string concatenation instead of list append

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,10 +2,9 @@
 
 def no_c(my_string):
     if my_string:
-        new_string = []
-        for i in range(len(my_string)):
+        new_string = ""
+        for i in range(0, len(my_string)):
             if (my_string[i] == 'c') or (my_string[i] == 'C'):
                 continue
-            new_string.append(my_string[i])
-        new_str = ''.join(new_string)
-        return new_str
+            new_string += my_string[i]
+        return new_string
