27128f27c79f6188de9aa841f6c9f7cd67c7b3cb
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-01-23 01:24:04 +0100

    Fix bug in no_c function

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,5 +6,6 @@ def no_c(my_string):
         for i in range(0, len(my_string)):
             if (my_string[i] == 'c') or (my_string[i] == 'C'):
                 continue
-            new_string += my_string[i]
+            else:
+                new_string += my_string[i]
         return new_string
