7c0d7caf3e2a2fc0aea6045f9d51fb16c471f37b
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-02-06 23:25:01 +0100

    Fix my_print() method in Square class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,10 +49,12 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+
         [print("") for i in range(0, self.__position[1])]
-        for i in range(1, self.__size + 1):
+        for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
-            print()
-        if self.__size == 0:
-            print()
+            print("")
