a2a1c02edd251e2823b4dc7914d0dd9b323216f6
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-02-06 22:04:15 +0100

    Add Square class and test cases

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" Square class """
+
+
+class Square:
+    """ Represent a square """
+    __size = 0
+    """ size of the square """
+    def __init__(self, size=0):
+        self.__size = size
+        try:
+            if type(size) != int:
+                raise TypeError
+            if size < 0:
+                raise ValueError
+        except TypeError:
+            raise TypeError('size must be an integer')
+        except ValueError:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        return self.__size ** 2
