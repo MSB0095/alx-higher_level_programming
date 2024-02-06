6fdf3ec8aca2c3d9eb14fe8cf3f83ec08d4ce7b0
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-02-06 21:47:05 +0100

    Add Square class and test instantiation

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" Square class """
+
+
+class Square:
+    """ Represent a square """
+    __size = 0
+    """ size of the square """
+    def __init__(self, size=0):
+        self.__size = size
