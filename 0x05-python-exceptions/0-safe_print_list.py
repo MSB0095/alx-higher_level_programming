eabd190d45a75d749d82522dbb82e870a9234270
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-02-07 18:44:49 +0100

    first commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" safe list printing
+"""
+def safe_print_list(my_list=[], x=0):
+    count = 0 
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
