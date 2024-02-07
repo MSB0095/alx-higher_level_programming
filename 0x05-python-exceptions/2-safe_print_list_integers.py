c90ce1b60fda35935b6d774320f5eca93f81d00f
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-02-07 19:04:14 +0100

    Add safe_print_list_integers function and test cases

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" safe list of integers printing
+"""
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            count += 1
+        except (TypeError, ValueError):
+            continue
+    print()
+    return count
