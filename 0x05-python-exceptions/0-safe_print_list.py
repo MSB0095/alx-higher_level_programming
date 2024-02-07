0d6aa974431e9825f4f809ca76490e6f4570123d
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-02-07 18:45:54 +0100

    pycodestyle fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,8 +2,10 @@
 
 """ safe list printing
 """
+
+
 def safe_print_list(my_list=[], x=0):
-    count = 0 
+    count = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
