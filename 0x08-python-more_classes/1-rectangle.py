db471b9172c2aac8561eb703ff069b50f9ee17d2
MSB0095 <andromedianmsb01@gmail.com>
2024-04-09 00:57:38 +0000

    Mise à jour de la classe Rectangle avec des méthodes pour définir la largeur et la hauteur

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,2 +1,33 @@
 #!/usr/bin/python3
+"""Defines a Rectangle object"""
 
+
+class Rectangle:
+    """
+    Rectangle Class
+    """
+    def __init__(self, width=0, height=0):
+        Rectangle.width(self, width)
+        Rectangle.height(self, height)
+
+    def width(self):
+        return self.width
+
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.width = value
+
+    def height(self):
+        return self.width
+
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.width = value
