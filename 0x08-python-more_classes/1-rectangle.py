58de493b0c38f539567639aa6aa5229f3f7c1090
MSB0095 <andromedianmsb01@gmail.com>
2024-04-09 01:03:50 +0000

    Mise à jour de la classe Rectangle avec des propriétés pour la largeur et la hauteur

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,27 +7,31 @@ class Rectangle:
     Rectangle Class
     """
     def __init__(self, width=0, height=0):
-        Rectangle.width(self, width)
-        Rectangle.height(self, height)
+        self.height = height
+        self.width = width
 
+
+    @property
     def width(self):
-        return self.width
+        return self.__width
 
+    @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.width = value
+        self.__width = value
 
+    @property
     def height(self):
-        return self.width
+        return self.__height
 
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         else:
-            self.width = value
+            self.__height = value
