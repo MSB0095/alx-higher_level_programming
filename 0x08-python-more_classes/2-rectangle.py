2335ab94aeb163c4af6b2b92dae2bb4c461e6202
MSB0095 <andromedianmsb01@gmail.com>
2024-04-09 01:17:29 +0000

    Ajouter la classe Rectangle avec les méthodes area() et perimeter()

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Defines a Rectangle object"""
+
+
+class Rectangle:
+    """
+    Rectangle Class
+    """
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return (int(self.width) * int(self.height))
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return (2 * (int(self.width))) + (2 * (int(self.height)))
