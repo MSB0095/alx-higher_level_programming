acaf709f9a4c8d9f70826a4c146d7c7ad9c3036b
MSB0095 <andromedianmsb01@gmail.com>
2024-05-26 20:05:04 +0100

    feat: Update SQL script to show the second_table with score and name in descending order

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- shows second_table
-SELECT second_table.score second_table.name FROM second_table ORDER BY second_table.score DESC;
+SELECT second_table.score, second_table.name FROM second_table ORDER BY second_table.score DESC;
