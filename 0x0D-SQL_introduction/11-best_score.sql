33df00be5bce3f97be430dd0a2454ee1f2b4f7bc
MSB0095 <andromedianmsb01@gmail.com>
2024-05-26 20:12:18 +0100

    feat: Update SQL script to show second_table users with a score greater than or equal to 10 and order them by score in descending order

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- shows users with score >= 10
-SELECT second_table.score, second_table.name FROM second_table WHERE second_table.score >= 10;
+SELECT second_table.score, second_table.name FROM second_table WHERE second_table.score >= 10 ORDER BY second_table.score DESC;
