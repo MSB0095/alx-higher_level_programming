3b445cdab64eb9a6e7c7b2b021a07f5ace5d995b
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-08-13 23:01:43 +0100

    Update lookup function docstring to provide a more descriptive explanation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def lookup(obj):
-    '''
-    Module for lookup method
-    '''
+    """This function returns the list of available attributes and methods of an object"""
     return dir(obj)
