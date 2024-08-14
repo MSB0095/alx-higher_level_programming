52dc9c966b75279e04ac0cee5e8e16bfe1b57818
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-08-14 23:07:17 +0100

    Refactor is_same_class function to use type() instead of isinstance()

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@
 def is_same_class(obj, a_class):
     """is_same_class function : returns true only when obj
     is an instance of a_class"""
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     else:
         return False
